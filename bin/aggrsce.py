866c732a09f86cc36152655210de292178957630
Hannes Ponstingl <hp3@sanger.ac.uk>
2021-10-22 18:50:46 +0100

    Moved to Pandas for data I/O. Removed minimum cell threshold. Revised consistency checking of gene names.

--- bin/aggrsce.py
+++ bin/aggrsce.py
@@ -1,22 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 DEBUG = True
 ## aggregate scRNA-seq expression levels per donor over all cells of one cell-type
 
 import sys
 import os
-import csv
 import argparse
 import gzip
 import numpy
 import anndata
 import pandas
 
-## input files
-DATA_DIR_HANDOVER = "/lustre/scratch123/hgi/projects/ukbb_scrna/pipelines/Pilot_UKB/handover/franke_data4"
-# franke data by default
-
-GENE_ANNOT_FILE = os.path.join(DATA_DIR_HANDOVER, "gene_annot.tsv")
+# input file format for gene annotations:
 # EnsEMBL gene region annotation downloaded as TSV file using BioMart
 # Format:
 # Gene stable ID  Gene stable ID version  Gene name       Chromosome/scaffold name        Gene start (bp) Gene end (bp)   Strand
@@ -24,107 +19,49 @@ GENE_ANNOT_FILE = os.path.join(DATA_DIR_HANDOVER, "gene_annot.tsv")
 # ENSG00000186827 ENSG00000186827.11      TNFRSF4 1       1211340 1214153 -1
 # ...
 
-CHRNAM_PREFIX_BED = 'chr' # prefix for chromosome names in BED output file
 STRTRANSL_SPACE_TO_UNDERSCORE = str.maketrans({' ':'_'})
-FMT_BEDFNAM_COUNT_MARIX = "count_{:s}.bed.gz"
-FMT_FNAM_CELLNUM_TABLE = "ncells_{:s}.tsv"
+FMT_BEDFNAM_COUNT_MARIX = "{:s}_{:s}_counts.bed.gz"
+FMT_FNAM_CELLNUM_TABLE = "{:s}_{:s}_ncells.tsv"
 FNAM_GENE_IDS = "gene_ids_without_annotation.txt"
-HEADER_BED_FILE = "chr\tstart\tend\tgene_id"
-HEADER_CELLNUM_TABLE = "experiment_id\tdonor_id\tsample_name\tcell_type\tn_cells\tn_cells_pass_qc\tdonor_included\tfile_name"
-FMT_CELLNUM_TABLE_ROW = "{0[0]:s}\t{0[1]:s}\t{0[2]:s}\t{0[3]:s}\t {0[4]:d}\t{0[5]:d}\t{0[6]}\t{0[7]:s}"
+HEADER_OUFLST = "index\tcell-type\tcounts_file\tcellnum_file"
 
-def set_argument_parser():
-    parser = argparse.ArgumentParser(description="Aggregate gene/transcript counts over cells of one class/type.")
-
-    parser.add_argument("--output-dir", "-o",
-        default=os.curdir,
-        help="output directory",
-        dest="outdir")
-
-    parser.add_argument("--gene-annotations", default = GENE_ANNOT_FILE,
-                        help="EnsEMBL/BioMart file of gene annotations [TSV]",
-                        dest="gene_annot_file")
-    parser.add_argument("--handover-data-dir", "-d", default = DATA_DIR_HANDOVER,
-                        help="Output directory of the Azimuth celltype-assignment pipeline.",
-                        dest="datadir_handover")
-    parser.add_argument("--gene-ids-output-file", default = None,
-                        help="Output file for gene ids.",
-                        dest="oufn_gene_ids")
-    return parser.parse_args()
-
-
-
-def load_gene_annotation_table(fnam):
-    gad = {}
-    if len(fnam) > 3 and fnam[-3:] == ".gz":
-        import gzip
-        infh = gzip.open(fnam, mode='rt')
-    else:
-        infh = open(fnam, 'r')
-    for row in csv.DictReader(infh, delimiter='\t'):
-        chrnam = row['chromosome']
-        start = int(row['start'])
-        end = int(row['end'])
-        id = row['query_id']
-        strand = int(row['strand'])
-        dt = (chrnam, start, end, strand, row['ensembl_id'])
-        try:
-            gad[id].append(dt)
-        except KeyError:
-            gad[id] = [dt]
-    infh.close()
-    return gad
-
+COUNT_MATRIX_COLUMN_DICT = {
+    "chromosome": "#chr",
+    "query_id": "gene_id"
+    }
 
 class DataHandover:
     def __init__(self):
         self.data_dir = ''
-        self.ftab = () # contains tuple of row dictionaries, row <-> donor
+        self.ftab = None # pandas data frame [experiment_id,donor_id,filename_h5ad,filename_annotation_tsv]
         self.anndata_objects = ()
-        self.celltypes = set()
-        self.gene_names = ()
+        self.cell_types = set()
+        self.gene_names = None
+        self.gene_annot_df = None
         self.failed_gene_idx = set()
 
     def load_file_table(self, data_dir, donor_only = True):
         self.data_dir = data_dir
-        ftab = []
-        fnam = os.path.join(self.data_dir, "files.tsv")
-
-        sys.stderr.write(
-            "# loading list of data files from {}\n".
-            format(fnam))
-
-        infh = open(fnam, 'r')
-        for row in csv.DictReader(infh, delimiter='\t'):
-            donor_id = row['donor_id']
-            if not donor_only or (donor_id != 'unassigned' and donor_id != 'doublet'):
-                ftab.append(row)
-        infh.close()
+        fnam = os.path.join(data_dir, "files.tsv")
+        self.ftab = pandas.read_table(fnam).set_index(["experiment_id", "donor_id"], drop = False)
+        if donor_only:
+            self.ftab.drop(index = ["doublet", "unassigned"], level=1, inplace = True)
 
         sys.stderr.write("# list of {:d} files loaded.\n".
-            format(len(ftab)))
-        self.ftab = tuple(ftab)
+                            format(len(self.ftab.shape))
+                            )
 
         return self.ftab
-
-    def load_count_files(self):
-        anndata_objects = []
-        for row in self.ftab:
-            fpath = os.path.join(self.data_dir, row['filename_h5ad'])
-            sys.stderr.write("# loading file {} ...\n".format(fpath))
-            anndata_objects.append(anndata.read_h5ad(fpath))
-        self.anndata_objects = tuple(anndata_objects)
-        return self.anndata_objects
 
     def get_cell_types(self):
         celltypes = set()
         for ad in self.anndata_objects:
             cts = set(pandas.Categorical(ad.obs['azimuth.celltyp.l2']).categories)
             celltypes = celltypes.union(cts)
-        self.celltypes = celltypes
-        return self.celltypes
+        return celltypes
 
     def check_gene_ids(self):
+        sys.stderr.write("# checking gene IDs across input files ...\n")
         gene_names_list = None
         for ad in self.anndata_objects:
             gene_names = ad.var.index
@@ -135,126 +72,112 @@ class DataHandover:
                 "ERROR: inconsistent gene name vector in file {}".
                 format(row['filename_h5ad'])
                 )
-        self.gene_names = tuple(gene_names_list)
-        return self.gene_names
+        return gene_names_list
+
+    def load_count_files(self):
+        anndata_objects = []
+        for i in self.ftab.index:
+            fpath = os.path.join(self.data_dir, self.ftab['filename_h5ad'][i])
+            sys.stderr.write("# loading file {} ...\n".format(fpath))
+            anndata_objects.append(anndata.read_h5ad(fpath))
+        self.anndata_objects = tuple(anndata_objects)
+        self.gene_names = self.check_gene_ids()
+        self.cell_types = self.get_cell_types()
+        return self.anndata_objects
 
     def check_gene_annot(self, gene_annotd):
         failed_gene_ids = []
         ctr = 0
         for gn in self.gene_names:
-            if gn not in gene_annotd:
+            if gn not in gene_annotd.index:
                 ctr += 1
                 sys.stderr.write("WARNING: [{:d}] gene {} not found in annotation.\n".format(ctr, gn))
                 failed_gene_ids.append(gn)
         sys.stderr.write("# {:d} out of {:d} genes have no annotation.\n".format(ctr, len(self.gene_names)))
-        self.failed_gene_ids = set(failed_gene_ids)
-        return len(self.failed_gene_ids)
+        return set(failed_gene_ids)
+
+    def add_gene_annotation(self, fnam):
+        sys.stderr.write(
+            "# loading EnsEMBL/BioMart gene annotations from file {}\n".
+            format(args.gene_annot_file))
+
+        self.gene_annot_df = pandas.read_table(fnam, index_col = 1)
+
+        sys.stderr.write(
+            "# annotations loaded for {:d} genes.\n".
+            format(self.gene_annot_df.shape[0])
+            )
+
+        self.failed_gene_ids = self.check_gene_annot(self.gene_annot_df)
+        return self.failed_gene_ids
 
     def _f_sum_over_celltype(self, ad, celltyp = 'all'):
-        selv_qcpass = ad.obs['qc.filter.pass'] == 'True'
+        sv_qcpass = ad.obs['qc.filter.pass'] == 'True'
         if celltyp is None or celltyp == 'all':
             # generate pseudo-bulk count
-            n_cells_celltype = sum(selv_qcpass)
+            n_cells_celltype = len(sv_qcpass)
         else:
             # cell-type specific count
-            selv_celltype = ad.obs['azimuth.celltyp.l2'] == celltyp
-            n_cells_celltype = len(selv_celltype)
-            selv_qcpass = selv_qcpass & selv_celltype
-        n_cells_qc = sum(selv_qcpass)
+            sv_celltype = ad.obs['azimuth.celltyp.l2'] == celltyp
+            n_cells_celltype = sum(sv_celltype)
+            sv_qcpass = sv_qcpass & sv_celltype
+        n_cells_qc = sum(sv_qcpass)
 
-        mr = numpy.sum(ad[selv_qcpass,].X, axis = 0, dtype = 'int')
-        return mr, n_cells_celltype, n_cells_qc
+        mr = numpy.sum(ad[sv_qcpass,].X, axis = 0, dtype = 'int')
+        df = pandas.DataFrame(mr, columns = ad.var.index)
+        # type(mr): <class 'numpy.matrix'>
+        return df, n_cells_celltype, n_cells_qc
 
     def write_failed_gene_ids(self, oufn = FNAM_GENE_IDS):
-        with open(oufh, 'w') as oufh:
+        with open(oufn, 'w') as oufh:
             for gn in self.failed_gene_ids:
                 oufh.write("{:s}\n".format(gn))
         return len(self.failed_gene_ids)
 
-    def write_cell_num_table(self, oufn, n_cells_per_donor_and_celltype):
-        oufh = open(oufn, 'w')
-        oufh.write(HEADER_CELLNUM_TABLE + '\n')
-        linctr = 0
-        for tup in n_cells_per_donor_and_celltype:
-            oufh.write(FMT_CELLNUM_TABLE_ROW.format(tup) + '\n')
-            linctr += 1
-        oufh.close()
-        return linctr
+    def sum_counts_over_celltype(self, celltype = 'all'):
+        count_df_lst = []
+        n_cells_celltype_lst = []
+        n_cells_qc_pass_lst = []
+        for i in range(len(self.ftab)):
+            row = self.ftab.iloc[i]
+            sum_df, n_cells_celltype, n_cells_qc = self._f_sum_over_celltype(self.anndata_objects[i], celltype)
+            count_df_lst.append(sum_df)
+            n_cells_celltype_lst.append(n_cells_celltype)
+            n_cells_qc_pass_lst.append(n_cells_qc)
+        counts_df = pandas.concat(count_df_lst, axis = 0, join = 'inner', ignore_index = True).set_index(self.ftab["donor_id"])
 
-    def write_count_matrix(self, oufn, count_mtx, sample_names, gene_annot_dict = None):
-        oufh = gzip.open(oufn, mode='wt')
-        headerstr = HEADER_BED_FILE
-        for snam in sample_names:
-            headerstr += "\t{:s}".format(snam)
-        oufh.write(headerstr + '\n')
-        n_genes = len(self.gene_names)
-        n_samples = len(sample_names)
-        linctr = 0
-        if count_mtx is None:
-            sys.stderr.write("WARNING: empty count matrix in file {:s}.\n".format(oufn))
-        else:
-            for g in range(n_genes):
-                gene_id = self.gene_names[g]
-                if gene_annot_dict is None:
-                    linstr = "{:s}".format(gene_id)
-                else:
-                    try:
-                        ga = gene_annot_dict[gene_id]
-                    except KeyError:
-                        continue
-                    else:
-                        mcol = count_mtx[:,g]
-                        if numpy.sum(mcol) < 1:
-                            continue # skip gene if there are no counts
-                        counts = list(mcol.flat)
-                        linctr += 1
-                        chrnam, start, end = ga[0][:3]
-                        midpos = int((start + end)/2) # 0-based for BED file
-                        linstr = "{:s}{}\t{:d}\t{:d}\t{:s}".format(CHRNAM_PREFIX_BED, chrnam, midpos, midpos+1, gene_id)
-                for s in range(n_samples):
-                    linstr += "\t{:d}".format(counts[s])
-                oufh.write(linstr + '\n')
-                linctr += 1
-        oufh.close()
-        return linctr
+        donor_df = pandas.DataFrame({
+            "donor_id": self.ftab["donor_id"],
+            "n_cells": n_cells_celltype_lst,
+            "n_cells_qc_pass": n_cells_qc_pass_lst
+            },
+            index = self.ftab.index
+        )
 
-    def sum_counts_over_celltype(self, celltype = 'all',  n_cells_threshold = 5,
-                                        gene_annotd = None, outdir = os.curdir):
-        n_cells_per_donor_and_celltype = []
+        return counts_df, donor_df
+
+    def aggregate_over_celltype(self, celltype = 'all', outdir = os.curdir, oufprfx = 'aggrsc'):
+        n_lines = 0
+        count_matrix, donor_df = self.sum_counts_over_celltype(celltype = celltype)
         celltypestr = celltype.translate(STRTRANSL_SPACE_TO_UNDERSCORE)
-        oufn = os.path.join(outdir, FMT_BEDFNAM_COUNT_MARIX.format(celltypestr))
-        count_matrix = None
-        n_donors = len(self.ftab)
-        sample_names = []
-        for i in range(n_donors):
-            r = self.ftab[i]
-            experiment_id = r['experiment_id']
-            donor_id = r['donor_id']
-            # sample_name = "{:s}.{:s}".format(experiment_id, donor_id)
-            sample_name = donor_id
-            mr, n_cells_celltype, n_cells_qc = self._f_sum_over_celltype(self.anndata_objects[i], celltype)
-            is_donor_included = n_cells_qc >= n_cells_threshold
-            n_cells_per_donor_and_celltype.append(
-                (experiment_id, donor_id, sample_name,
-                    celltype, n_cells_celltype, n_cells_qc, is_donor_included, oufn)
-                )
-            if is_donor_included:
-                sample_names.append(sample_name)
-                if count_matrix is None:
-                    count_matrix = mr
-                else:
-                    count_matrix = numpy.concatenate((count_matrix, mr), axis = 0)
+        oufn_matrix = os.path.join(outdir, FMT_BEDFNAM_COUNT_MARIX.format(oufprfx, celltypestr))
+        oufn_donor_table = os.path.join(outdir, FMT_FNAM_CELLNUM_TABLE.format(oufprfx, celltypestr))
         if count_matrix is None:
             sys.stderr.write("WARNING: no counts for celltype {:s}\n".format(celltypestr))
-        linctr = self.write_count_matrix(oufn, count_matrix, sample_names, gene_annotd)
-        sys.stderr.write("# {:d} lines writen to file {:s} ...\n".format(linctr, oufn))
+        else:
+            anno_mt = pandas.concat(
+                [dh.gene_annot_df[["chromosome", "start", "end", "query_id"]].rename(COUNT_MATRIX_COLUMN_DICT, axis = 1),
+                count_matrix.T],
+                axis = 1,
+                join = "inner" # only the genes that are annotated
+                )
+            n_lines = len(anno_mt)
+            anno_mt.to_csv(oufn_matrix, sep = "\t")
+            sys.stderr.write("# {:d} lines written to file {:s} ...\n".format(n_lines, oufn_matrix))
 
-        n_cells_per_donor_and_celltype = tuple(n_cells_per_donor_and_celltype)
-        oufn = os.path.join(outdir, FMT_FNAM_CELLNUM_TABLE.format(celltypestr))
-        linctr = self.write_cell_num_table(oufn, n_cells_per_donor_and_celltype)
-        sys.stderr.write("# {:d} lines writen to file {:s} ...\n".format(linctr, oufn))
-
-        return n_cells_per_donor_and_celltype
+        donor_df.to_csv(oufn_donor_table, sep = '\t')
+        sys.stderr.write("# {:d} lines written to file {:s} ...\n".format(len(donor_df), oufn_donor_table))
+        return n_lines, oufn_matrix, oufn_donor_table
 
 def set_argument_parser():
     parser = argparse.ArgumentParser(description="Aggregate gene/transcript counts over cells of one class/type.")
@@ -264,57 +187,66 @@ def set_argument_parser():
         help="output directory",
         dest="outdir")
 
-    parser.add_argument("--gene-annotations", default = GENE_ANNOT_FILE,
+    parser.add_argument("--gene-annotations", required = True,
                         help="EnsEMBL/BioMart file of gene annotations [TSV]",
                         dest="gene_annot_file")
-    parser.add_argument("--handover-data-dir", "-d", default = DATA_DIR_HANDOVER,
+    parser.add_argument("--handover-data-dir", "-d", default = os.curdir,
                         help="Output directory of the Azimuth celltype-assignment pipeline.",
                         dest="datadir_handover")
     parser.add_argument("--gene-ids-output-file", default = None,
                         help="Output file for gene ids for which there was no annotation.",
                         dest="oufn_gene_ids")
-    parser.add_argument("--min-cell-num", type = int, default = int(5),
-                        help="Minimum number of cells per donor.",
-                        dest="n_cells_min")
+    parser.add_argument("--output-file-list", default = None,
+                        help="Write list of celltypes and corresponding ouput files.",
+                        dest="oufn_filelist")
+    parser.add_argument("--output-file-prefix", default = 'aggrsce',
+                        help="Prefix for output files.",
+                        dest="oufn_prfx")
     return parser.parse_args()
 
+class TestArgs:
+    def __init__(self):
+        self.gene_annot_file = 'gene_annot.tsv'
+        self.datadir_handover = 'franke_data4'
+        self.oufn_gene_ids = 'gene_ids_unannotated.txt'
 
 if __name__ == '__main__':
+    # args = TestArgs()
     args = set_argument_parser()
     if args.outdir != os.curdir and not os.access(args.outdir, os.F_OK):
         os.mkdir(args.outdir)
 
-    sys.stderr.write(
-        "# loading EnsEMBL/BioMart gene annotations from file {}\n".
-        format(args.gene_annot_file))
-    gannotd = load_gene_annotation_table(args.gene_annot_file)
-    sys.stderr.write(
-        "# annotations loaded for {:d} genes.\n".
-        format(len(gannotd)))
+    if DEBUG:
+        sys.stderr.write("current directory: {}\n".format(os.path.abspath(os.curdir)))
+        for fn in os.listdir():
+            sys.stderr.write("- ls: {}\n".format(fn))
+        for fp in os.listdir(args.datadir_handover):
+            sys.stderr.write("-- ls: {}\n".format(fp))
+
+    if args.oufn_filelist:
+        oufh_flst = open(args.oufn_filelist, 'w')
+        oufh_flst.write(HEADER_OUFLST + '\n')
+    else:
+        oufh_flst = sys.sterr
 
     dh = DataHandover()
     dh.load_file_table(args.datadir_handover, donor_only = True)
     dh.load_count_files()
-    dh.check_gene_ids()
-    dh.check_gene_annot(gene_annotd = gannotd)
-    if (args.oufn_gene_ids):
-        dh.write_failed_gene_ids(path.join(args.outdir, args.oufn_gene_ids))
-    celltypes = dh.get_cell_types()
+    dh.add_gene_annotation(args.gene_annot_file)
+    if args.oufn_gene_ids:
+        dh.write_failed_gene_ids(os.path.join(args.outdir, args.oufn_gene_ids))
     # print(celltypes)
-    dh.sum_counts_over_celltype(
-        "all",
-        n_cells_threshold = args.n_cells_min,
-        gene_annotd = gannotd,
-        outdir = args.outdir
-        )
-
-    for ct in celltypes:
-        sys.stderr.write("processing celltype '{:s}' ...\n".format(ct))
-        dh.sum_counts_over_celltype(
-            ct,
-            n_cells_threshold = args.n_cells_min,
-            gene_annotd = gannotd,
-            outdir = args.outdir
+    ctr = 0
+    celltypes = ['all'] + list(dh.cell_types)
+    for celltyp in celltypes:
+        sys.stderr.write("processing celltype '{:s}' ...\n".format(celltyp))
+        n_lines, oufn_matrix, oufn_donor_table = \
+        dh.aggregate_over_celltype(
+            celltyp,
+            outdir = args.outdir,
+            oufprfx = args.oufn_prfx
             )
+        oufh_flst.write(f'{ctr}\t{celltyp}\t{oufn_matrix}\t{oufn_donor_table}\n')
+        ctr += 1
 
     exit(0)
