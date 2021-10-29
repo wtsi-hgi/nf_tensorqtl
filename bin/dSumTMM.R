0d8fe1c551d35283facb7ec179a406418aa2db33
Hannes Ponstingl <hp3@sanger.ac.uk>
2021-10-29 16:25:42 +0100

    Fixed issue that led to donors being dropped from normalized matrix and PCA returing errors. Caused by integer arguments to script being interpreted as char.

--- bin/dSumTMM.R
+++ bin/dSumTMM.R
@@ -5,6 +5,8 @@
 
 ## assume there are donors to be excluded who have a number of cells below a specified threshold
 
+DEBUG = TRUE
+
 DEFAULT_MIN_CELLS_PER_DONOR <- 5 # remove samples with fewer than this number of cells
 DEFAULT_NUM_PCS_WRITTEN <- 20 # number of principal components to write out
 COLUMN_GENE_ID <- 4
@@ -46,12 +48,13 @@ write.principal.components <- function(df, dirnam, n.pcs = DEFAULT_NUM_PCS_WRITT
   if (nz > 0) {
     m <- m[, which(z.v)]
     }
-  pca <- prcomp(m, center = TRUE, scale. = TRUE)
-  nc <- dim(pca$x)[1]
-  if (nc > n.pcs) {
-    nc <- n.pcs
-  }
-  write.table(pca$x[,1:nc], file = oufn, sep = "\t", row.names = TRUE, quote = FALSE)
+  pca <- prcomp(m, center = TRUE, scale. = TRUE, rank. = n.pcs)
+  ## nc <- dim(pca$x)[1]
+  ##if (nc > n.pcs) {
+  ##  nc <- n.pcs
+  ##}
+  ##write.table(pca$x[,1:nc], file = oufn, sep = "\t", row.names = TRUE, quote = FALSE)
+  write.table(pca$x, file = oufn, sep = "\t", row.names = TRUE, quote = FALSE)
   pca
 }
 
@@ -62,10 +65,22 @@ normalise.dSumTMM <- function(phenotype.bed.file, donor.tsv.file, min.cells.per.
   # read table with the number of cells per donor
   df.donors <- read.delim(donor.tsv.file, row.names = NULL, quote = "")
 
-  sample.names.included <- df.donors$donor_id[
-    df.donors$is_included == "True" &
-    df.donors$n_cells_qc_pass >= min.cells.per.donor
-  ]
+  s1.v <- as.logical(df.donors$is_included)
+  s2.v <- df.donors$n_cells_qc_pass >= min.cells.per.donor
+  if (DEBUG) {
+    cat("donors included:", sum(s1.v), "out of", length(s1.v),
+      "\ndonors with a minimum of", min.cells.per.donor,"cells:",sum(s2.v),
+      "\nNumber of donors combined:", sum(s1.v & s2.v),
+      "\nmin.cells.per.donor =", min.cells.per.donor,
+      "\nclass(df.donors$n_cells_qc_pass) = ", class(df.donors$n_cells_qc_pass),
+      "\nclass(min.cells.per.donor) = ", class(min.cells.per.donor),
+      "\n"
+      )
+    ddf <- data.frame(df.donors, "is.read.included" = s1.v, "is.checked.qc" = s2.v)
+    write.table(ddf, file = "DEBUG_OUT.tsv", sep = "\t", row.names = F, quote = FALSE)
+  }
+  # data.frame("donor_id" = df.donors$donor_id, "is.included" = sel.v)
+  sample.names.included <- df.donors$donor_id[s1.v & s2.v]
 
   dat.counts <- read.delim(phenotype.bed.file, row.names = NULL, quote = "")
   row.names(dat.counts) <- dat.counts[,COLUMN_GENE_ID]
@@ -73,8 +88,14 @@ normalise.dSumTMM <- function(phenotype.bed.file, donor.tsv.file, min.cells.per.
 
   n_cols = dim(dat.counts)[2]
   dm <- as.matrix(dat.counts[,(COLUMN_GENE_ID+1):n_cols])
+
   ## subset matrix to remove donors which fewer than MIN_CELLS_PER_DONOR cells
   dm <- dm[,as.vector(sample.names.included)]
+  donor.names.in <- colnames(dm)
+  n.donors = dim(dm)[2]
+  if (DEBUG) {
+    cat("Number of donors in data matrix:", n.donors,"\n")
+  }
   ## subset matrix to remove unobserved genes
   sv.nonzero.genes = rowSums(dm) != 0
   dm <- dm[sv.nonzero.genes,]
@@ -93,13 +114,23 @@ normalise.dSumTMM <- function(phenotype.bed.file, donor.tsv.file, min.cells.per.
 
   # log2(CPM) as output
   m.logcpm <- edgeR::cpm(dge, log=TRUE)
+  donor.names.out = colnames(m.logcpm)
+  n.donors.out <- dim(m.logcpm)[2]
+  if (DEBUG) {
+    cat("Number of donors in normalized data matrix:", n.donors.out,"\n")
+  }
 
-  df <- cbind(dat.counts[sv.nonzero.genes,1:COLUMN_GENE_ID], as.data.frame(m.logcpm))
+  list(
+    "data" = cbind(dat.counts[sv.nonzero.genes,1:COLUMN_GENE_ID], as.data.frame(m.logcpm)),
+    "donors" = data.frame("donor.id" = donor.names.in, "is.norm" =  donor.names.in %in% donor.names.out)
+    )
 }
 
 ## main
 ## infnam.bed <- "aggrsum_all_counts.bed.gz"
 ## infnam.tsv <- "aggrsum_all_ncells.tsv"
+sessionInfo()
+.Machine
 args.v <- commandArgs(trailingOnly = TRUE)
 cat("invoked R like this:\n");
 cat(paste(args.v, collapse=" "), "\n")
@@ -111,16 +142,16 @@ if (args.n > 1 & args.n < 6) {
   n.pcs <- 0
   output_is_perChr <- FALSE
   if (args.n > 2) {
-    n.cells.min <- args.v[3]
+    n.cells.min <- as.integer(args.v[3]) ## as.integer() is important here!
     if (args.n > 3) {
-      n.pcs <- args.v[4]
+      n.pcs <- as.integer(args.v[4]) ## as.integer() is important here!
       if (args.n > 4) {
         output_is_perChr <- args.v[5] == 'perChr'
       }
     }
   }
 } else {
-  stop("Call with <input BED file> <input TSV table> <min_cell_mum> <n_PCs> [perChr]\n", call.=FALSE)
+  stop("Call with <input BED file> <input TSV table> <min_cell_mum> <n_pcs> ['perChr']\n", call.=FALSE)
 }
 
 cat ("bed file:", infnam.bed, ", tsv table:", infnam.tsv, "n.cells.min =", n.cells.min, "n.pcs =", n.pcs, "\n")
@@ -129,19 +160,27 @@ if (output_is_perChr && !file.exists(dirnam)) {
   dir.create(dirnam)
 }
 
-df <- normalise.dSumTMM(
+df.norm <- normalise.dSumTMM(
   phenotype.bed.file = infnam.bed,
   donor.tsv.file = infnam.tsv,
   min.cells.per.donor = n.cells.min
   )
 if (n.pcs > 0) {
   cat("write", n.pcs, "principal components ...\n")
-  pca <- write.principal.components(df, dirnam, n.pcs = n.pcs)
+  pca <- write.principal.components(df.norm$data, dirnam, n.pcs = n.pcs)
 }
 
 if (output_is_perChr) {
-  write.perChrBED(df, dirnam)
+  write.perChrBED(df.norm$data, dirnam)
 } else {
-  write.singleBED(df, oufn = paste0(dirnam, "_chrAll.bed"))
+  write.singleBED(df.norm$data, oufn = paste0(dirnam, "_chrAll.bed"))
 }
+
+# output table of donor input names and whether or not they are included in
+# normalized matrix
+write.table(
+  df.norm$donors,
+  file = paste0(dirnam, "_donors.tsv"),
+  sep = "\t", row.names = F, quote = FALSE
+  )
 cat("finished.")
